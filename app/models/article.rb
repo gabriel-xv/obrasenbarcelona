b95bfe0808a8fcd79e0ea2ed40fdebe19d4b288c
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2021-08-11 12:31:07 +0200

    Articles associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+
+  CATEGORIES = ['Baños', 'Cocinas', 'Fachadas', 'Instalaciones', 'Reformas Integrales', 'Suelos', 'Terrazas'].freeze
+
+  validates_presence_of :title, :description, :category
+  validates :category, inclusion: { in: CATEGORIES }
 end
