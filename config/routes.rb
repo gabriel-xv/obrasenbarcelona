1ebcff2fd9d57bd3daf4e89609db2fc593f373a1
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2021-09-28 22:36:03 +0200

    Mail form setup for contacts, testing with mailcatcher and some form style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
   root to: 'pages#home'
   resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :contacts, only: [:new, :create]
 end
