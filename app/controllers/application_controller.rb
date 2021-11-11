d62a629ccb9486d41a845f043f19595a8a896545
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2021-11-11 19:32:14 +0100

    default errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    not_found
+  end
+
+  rescue_from ActionController::RoutingError do
+    not_found
+  end
+
+  rescue_from ActionController::StandardError do
+    internal_server_error
+  end
 end
